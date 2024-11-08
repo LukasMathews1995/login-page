558dee0fd60fe7760c2c71c7b26b2e8d1ab55e14
Lucas Mathews <lucasmathews50@gmail.com>
2024-11-07 22:04:19 -0300

    feat: create singup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/sign-up/signup.component';
 
 export const routes: Routes =[ {
     path:"login",
     component:LoginComponent
-}];
+},{path:"signup",
+    component:SignupComponent}
+
+
+];
